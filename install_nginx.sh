fcd90bd2916418e1901491b78d4dac26320a27f7
chujq <chujiaqiang@outlook.com>
2015-12-23 10:50:25 +0800

    user the variables to replace absoulte path

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,6 +1,14 @@
+#/bin/sh
+
+# Name: install_nginx_sh
+# This scripts use to installl nginx on ubuntu, It will download gninx and related packages first
+# and then install them automatically.
+#
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+
+WORK_PATH='/fmt'
 
 # Check if user is root
 if [ $(id -u) != "0" ]; then
@@ -11,63 +19,63 @@ fi
 groupadd www
 useradd -s /sbin/nologin -g www www
 
-rm -Rf /fmt
-mkdir /fmt
-mkdir /fmt/packages
-mkdir /fmt/data
-mkdir /fmt/server
-mkdir /fmt/log
+rm -Rf ${WORK_PATH}
+mkdir -p ${WORK_PATH}
+mkdir -p ${WORK_PATH}/packages
+mkdir -p ${WORK_PATH}/data
+mkdir -p ${WORK_PATH}/server
+mkdir -p ${WORK_PATH}/log
 
+cd ${WORK_PATH}/packages
 
-cd /fmt/packages
-
+URL_PCRE=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.37.tar.gz
+URL_ZLIB=http://zlib.net/zlib-1.2.8.tar.gz
+URL_OPENSSL=http://www.openssl.org/source/openssl-1.0.2e.tar.gz
+URL_NGINX=http://nginx.org/download/nginx-1.8.0.tar.gz
 
 #refere to https://www.nginx.com/resources/admin-guide/installing-nginx-open-source/
 
 #the PCRE library – required for the Core and Rewrite modules and provides support for regular expressions:
-wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.37.tar.gz
+wget $URL_PCRE
 tar -zxf pcre-8.37.tar.gz
 cd pcre-8.37
 ./configure
 make && make install
-
 cd ..
 
 
 #the zlib library – required by the Gzip module for headers compression:
-wget http://zlib.net/zlib-1.2.8.tar.gz
+wget $URL_ZLIB
 tar -zxf zlib-1.2.8.tar.gz
 cd zlib-1.2.8
 ./configure
 make && make install
-
 cd ..
 
 
 #the OpenSSL library – required the SSL module to support the HTTPS protocol:
-wget http://www.openssl.org/source/openssl-1.0.2e.tar.gz
+wget $URL_OPENSSL
 tar zxvf openssl-1.0.2e.tar.gz
 cd openssl-1.0.2e
 ./config
 make && make install
-
 cd ..
 
 #install the Nignx
-wget http://nginx.org/download/nginx-1.8.0.tar.gz
+wget $URL_NGINX
 tar zxvf nginx-1.8.0.tar.gz
 cd nginx-1.8.0
 
 ./configure \
         --user=www \
         --group=www \
-        --prefix=/fmt/server/nginx \
-        --conf-path=/fmt/server/nginx/nginx.conf \
-        --pid-path=/fmt/server/nginx/run/nginx.pid \
-        --lock-path=/fmt/server/nginx/run/nginx.lock \
-        --sbin-path=/fmt/server/nginx/sbin/nginx \
-        --error-log-path=/fmt/log/nginx/error.log \
-        --http-log-path=/fmt/log/nginx/access.log \
+        --prefix=${WORK_PATH}/server/nginx \
+        --conf-path=${WORK_PATH}/server/nginx/nginx.conf \
+        --pid-path=${WORK_PATH}/server/nginx/run/nginx.pid \
+        --lock-path=${WORK_PATH}/server/nginx/run/nginx.lock \
+        --sbin-path=${WORK_PATH}/server/nginx/sbin/nginx \
+        --error-log-path=${WORK_PATH}/log/nginx/error.log \
+        --http-log-path=${WORK_PATH}/log/nginx/access.log \
         --with-http_gzip_static_module \
         --with-http_ssl_module \
         --with-ipv6 \
@@ -92,12 +100,12 @@ mkdir -p /var/cache/nginx/fastcgi_temp
 mkdir -p /var/cache/nginx/uwsgi_temp
 mkdir -p /var/cache/nginx/scgi_temp
 
-cd /fmt/server/nginx/sbin/
-./nginx -c /fmt/server/nginx/nginx.conf
+cd ${WORK_PATH}/server/nginx/sbin/
+./nginx -c ${WORK_PATH}/server/nginx/nginx.conf
 
 
 #http://nginx.org/en/linux_packages.html?_ga=1.138264499.2092786214.1447975135
-		
+
 #Configuration summary
 #  + using system PCRE library
 #  + using system OpenSSL library
@@ -105,13 +113,13 @@ cd /fmt/server/nginx/sbin/
 #  + sha1: using OpenSSL library
 #  + using system zlib library
 #
-#  nginx path prefix: "/fmt/server/nginx"
-#  nginx binary file: "/fmt/server/nginx/sbin/nginx"
-#  nginx configuration prefix: "/fmt/server/nginx"
-#  nginx configuration file: "/fmt/server/nginx/nginx.conf"
-#  nginx pid file: "/fmt/server/nginx/run/nginx.pid"
-#  nginx error log file: "/fmt/log/nginx/error.log"
-#  nginx http access log file: "/fmt/log/nginx/access.log"
+#  nginx path prefix: "${WORK_PATH}/server/nginx"
+#  nginx binary file: "${WORK_PATH}/server/nginx/sbin/nginx"
+#  nginx configuration prefix: "${WORK_PATH}/server/nginx"
+#  nginx configuration file: "${WORK_PATH}/server/nginx/nginx.conf"
+#  nginx pid file: "${WORK_PATH}/server/nginx/run/nginx.pid"
+#  nginx error log file: "${WORK_PATH}/log/nginx/error.log"
+#  nginx http access log file: "${WORK_PATH}/log/nginx/access.log"
 #  nginx http client request body temporary files: "/var/cache/nginx/client_temp"
 #  nginx http proxy temporary files: "/var/cache/nginx/proxy_temp"
 #  nginx http fastcgi temporary files: "/var/cache/nginx/fastcgi_temp"
@@ -121,7 +129,7 @@ cd /fmt/server/nginx/sbin/
 
 #issue
 #root@ECS/etc/init.d/> ./nginx -s reload
-#nginx: [error] open() "/fmt/server/nginx/run/nginx.pid" failed (2: No such file or directory)
+#nginx: [error] open() "${WORK_PATH}/server/nginx/run/nginx.pid" failed (2: No such file or directory)
 #Solution:
-#Use: /fmt/server/nginx/sbin/nginx -c /fmt/server/nginx/nginx.conf
+#Use: ${WORK_PATH}/server/nginx/sbin/nginx -c ${WORK_PATH}/server/nginx/nginx.conf
 
