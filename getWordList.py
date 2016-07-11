8238d177866c6d69ba658b77144b34957da89b46
JiaQiang Chu <chujiaqiang@outlook.com>
2016-07-11 17:32:06 +0800

    Add files via upload

--- getWordList.py
+++ getWordList.py
@@ -2,7 +2,6 @@
 
 import urllib
 import re
-from bs4 import 
 
 #get word's info from http://dict.cn
 url=r'http://dict.cn/home'
@@ -12,9 +11,10 @@ html=urllib.urlopen(url).read()
 f = open("C:\\Users\\IBM_ADMIN\\Desktop\\Hadoop- The Definitive Guide, 4th Edition.txt")
 dict = {}
 for line in f.readlines():
-    #line = f.readline()
+    line = line.lower().strip()
+    
     p = re.compile(r'\W+')
-    arry = p.split(line.lower().strip())
+    arry = p.split(line)
     for word in arry:
         if dict.has_key(word):
             dict[word] += 1
@@ -23,8 +23,15 @@ for line in f.readlines():
 f.close()
 
 f =open("C:\\Users\\IBM_ADMIN\\Desktop\\Hadoop- The Definitive Guide, 4th Edition.lst",'wb+')
+del dict[""]
 for item in sorted(dict.iteritems(), key = lambda asd:asd[1], reverse = True):
-    f.write("%s\t%d\n" % (item[0],item[1]))
+    worditem = "%s\t%d\n" % (item[0],item[1])
+    f.write(worditem)
+    #print worditem
+    #url='%s%s' % (r'http://dict.cn/home',item[0])
+    #html=urllib.urlopen(url).read()
 f.close()
+
+
 
     
